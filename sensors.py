2aa549cbc986515421dd6907f3ca26863e73f719
syro13 <lowisjakub@gmail.com>
2023-11-22 23:31:29 +0000

    Sensor code complete

--- sensors.py
+++ sensors.py
@@ -1,1 +1,16 @@
+import board
+import busio
+import adafruit_ads1x15.ads1115 as ADS
+from adafruit_ads1x15.analog_in import AnalogIn
 
+i2c = busio.I2C(board.SCL, board.SDA)
+
+ads = ADS.ADS1115(i2c)
+
+channel_turb = AnalogIn(ads, ADS.P0)
+channel_tds = AnalogIn(ads, ADS.P1)
+channel_ph = AnalogIn(ads, ADS.P2)
+
+
+while True:
+    print("turb: ", channel_turb.value, "V: ", channel_turb.voltage, "|tds: ", channel_tds.value, "V:", channel_tds.voltage, "ph: ", channel_ph.value, "V: ", channel_ph.voltage)
